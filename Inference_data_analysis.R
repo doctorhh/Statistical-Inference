b8c928432bca70286eab0c557ad69fc66af03064
Hans <hans.harvey@h2hteam.com>
2015-12-22 07:29:13 -0500

    Little update/modification

--- Inference_data_analysis.R
+++ Inference_data_analysis.R
@@ -12,14 +12,13 @@ table(ToothGrowth$supp,ToothGrowth$dose)
 
 interaction.plot(ToothGrowth$dose, ToothGrowth$supp, ToothGrowth$len)
 
-table(ToothGrowth$supp,ToothGrowth$dose)
-
 q <- ggplot(ToothGrowth,aes(factor(dose),len)) +
       geom_boxplot(aes(fill=dose)) + facet_grid(.~supp)
 print(q)
 
 stats_df <-ToothGrowth %>%
       group_by(supp,dose) %>%
-      summarise(mean_Len = mean(len), stddev_Len = sd(len), n = n()) %>%
-      mutate(lwr_limit = mean_Len - 2 * stddev_Len/sqrt(n)) %>%
-      mutate(upr_limit = mean_Len + 2 * stddev_Len/sqrt(n))
+      summarise(mean_Len = mean(len), stddev_Len = sd(len), n = n()) 
+#%>%
+#      mutate(lwr_limit = mean_Len - 2 * stddev_Len/sqrt(n)) %>%
+#      mutate(upr_limit = mean_Len + 2 * stddev_Len/sqrt(n))
