e190a6689a1ba9348c40f072c55703499f4c84ed
Hans <hans.harvey@h2hteam.com>
2015-12-22 06:48:25 -0500

    Graph and summary instruction

--- Inference_data_analysis.R
+++ Inference_data_analysis.R
@@ -2,9 +2,24 @@ library(dplyr)
 library(ggplot2)
 data(ToothGrowth)
 
-srt(ToothGrowth)
+str(ToothGrowth)
 head(ToothGrowth)
 nrow(ToothGrowth)
 
 summary(ToothGrowth)
 sum(is.na(ToothGrowth))
+table(ToothGrowth$supp,ToothGrowth$dose)
+
+interaction.plot(ToothGrowth$dose, ToothGrowth$supp, ToothGrowth$len)
+
+table(ToothGrowth$supp,ToothGrowth$dose)
+
+q <- ggplot(ToothGrowth,aes(factor(dose),len)) +
+      geom_boxplot(aes(fill=dose)) + facet_grid(.~supp)
+print(q)
+
+stats_df <-ToothGrowth %>%
+      group_by(supp,dose) %>%
+      summarise(mean_Len = mean(len), stddev_Len = sd(len), n = n()) %>%
+      mutate(lwr_limit = mean_Len - 2 * stddev_Len/sqrt(n)) %>%
+      mutate(upr_limit = mean_Len + 2 * stddev_Len/sqrt(n))
