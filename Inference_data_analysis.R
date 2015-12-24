e0ec1ec8945ee6a133b7f2a2e2fdef2c342a060c
Hans <hans.harvey@h2hteam.com>
2015-12-24 08:40:13 -0500

    Calculation completed - knit file outstanding (PDF)

--- Inference_data_analysis.R
+++ Inference_data_analysis.R
@@ -18,7 +18,25 @@ print(q)
 
 stats_df <-ToothGrowth %>%
       group_by(supp,dose) %>%
-      summarise(mean_Len = mean(len), stddev_Len = sd(len), n = n()) 
-#%>%
-#      mutate(lwr_limit = mean_Len - 2 * stddev_Len/sqrt(n)) %>%
-#      mutate(upr_limit = mean_Len + 2 * stddev_Len/sqrt(n))
+      summarise(mean_Len = mean(len), stddev_Len = sd(len), n = n())
+
+stats_df %>% group_by(dose) %>% summarise(mean_d=mean(mean_Len))
+stats_df %>% group_by(supp) %>% summarise(mean_d=mean(mean_Len))
+
+#T.Test of supplement type
+t_VC <- ToothGrowth %>% filter(supp == "VC") %>% select(len)
+t_OJ <- ToothGrowth %>% filter(supp == "OJ") %>% select(len)
+t.test(t_OJ,t_VC,paired = FALSE, var.equal = FALSE)
+
+#T.Test of dose & supplement type
+t_d0 <- ToothGrowth %>% filter(dose == 0.5)
+t_d1 <- ToothGrowth %>% filter(dose == 1)
+t_d2 <- ToothGrowth %>% filter(dose == 2)
+
+#t.test(t_d0,t_d1,paired = FALSE, var.equal = TRUE)
+#t.test(t_d1,t_d2,paired = FALSE, var.equal = TRUE)
+#t.test(t_d0,t_d2,paired = FALSE, var.equal = TRUE)
+
+t.test(filter(t_d0,supp=="OJ")$len,filter(t_d0,supp=="VC")$len ,paired = FALSE, var.equal = TRUE)
+t.test(filter(t_d1,supp=="OJ")$len,filter(t_d1,supp=="VC")$len ,paired = FALSE, var.equal = TRUE)
+t.test(filter(t_d2,supp=="OJ")$len,filter(t_d2,supp=="VC")$len ,paired = FALSE, var.equal = TRUE)
